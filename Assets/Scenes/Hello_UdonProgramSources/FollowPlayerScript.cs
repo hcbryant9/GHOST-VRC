c1542ce031f4ee94a5c9036eea9e2b44ecc949d2
hcbryant9 <bryanthc@vcu.edu>
2023-10-02 22:38:10 -0400

    adding sarah's fbx and ghost cube
    a cube will now follow u around spookily

---
+++ Assets/Scenes/Hello_UdonProgramSources/FollowPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class FollowPlayerScript : UdonSharpBehaviour
+{
+    private Vector3 playerPosition;
+    public float moveSpeed = 5.0f; // Speed at which the cube follows the player
+
+    private void Start()
+    {
+        // Get the initial position of the local player's avatar
+        playerPosition = Networking.LocalPlayer.GetPosition();
+    }
+
+    private void Update()
+    {
+        //Vector3 distanceToPlayer = playerPosition - transform.position;
+        //float distanceToPlayerF = distanceToPlayer.magnitude;
+        
+       
+        playerPosition = Networking.LocalPlayer.GetPosition();
+        Vector3 direction = playerPosition - transform.position;
+        direction.Normalize();
+        transform.position += direction * moveSpeed * Time.deltaTime;
+        
+
+        
+      
+        
+    }
+}
