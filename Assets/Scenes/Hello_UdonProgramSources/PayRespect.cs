54120698f1faf151045225cf12b892ec4abdac09
hcbryant9 <bryanthc@vcu.edu>
2023-10-03 20:09:51 -0400

    adding text interaction (attempt)

---
+++ Assets/Scenes/Hello_UdonProgramSources/PayRespect.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PayRespect : UdonSharpBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    void DestroySelf()
+    {
+        gameObject.SetActive(false);
+        Destroy(gameObject, 1f);
+    }
+    void OnDrop()
+    {
+        // show some new text
+        // disable object
+        Debug.Log("Show some text and disable the object");
+        DestroySelf();
+        // waut for some seconds
+    }
+
+    
+}
