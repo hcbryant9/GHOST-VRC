ee7cd6ad1cb170f083a9096eac9124f184fe92bd
hcbryant9 <bryanthc@vcu.edu>
2023-10-04 13:22:44 -0400

    adding text and type writer
    In the code, the messages from the ghost will be typed out

---
+++ Assets/Scenes/Hello_UdonProgramSources/GhostScript.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class GhostScript : UdonSharpBehaviour
+{
+    public TypeWriter writer;
+    void Start()
+    {
+        SendScript();
+    }
+    public void SendScript()
+    {
+        if (writer != null)
+        {
+            writer.Write("hello world");
+        } else
+        {
+            Debug.Log("Writer is null");
+        }
+        
+    }
+}
---
+++ Assets/Scenes/Hello_UdonProgramSources/TypeWriter.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class TypeWriter : UdonSharpBehaviour
+{
+    public TMPro.TextMeshProUGUI textObject;
+    
+    public void Write(string message)
+    {
+        if(textObject != null)
+        {
+            textObject.text = message;
+        } else
+        {
+            Debug.Log("TMPro object is empty");
+        }
+        
+    }
+}
