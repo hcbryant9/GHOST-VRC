e9ad7c8cdfa11ebfdb887a27dd557e55a9ac7949
hcbryant9 <bryanthc@vcu.edu>
2023-10-16 12:09:33 -0400

    working on how the grandma follows the player

--- Assets/02_Scenes/Hello_UdonProgramSources/FollowPlayerScript.cs
+++ Assets/02_Scenes/Hello_UdonProgramSources/FollowPlayerScript.cs
@@ -5,12 +5,13 @@ using VRC.SDKBase;
 public class FollowPlayerScript : UdonSharpBehaviour
 {
     private Vector3 playerPosition;
-    public float moveSpeed = 5.0f; // Speed at which the cube follows the player
-
+    public float moveSpeed = 1.0f; // Speed at which the cube follows the player
+    public float maxDistance = 5.0f; // Maximum distance from the player
     private void Start()
     {
         // Get the initial position of the local player's avatar
         playerPosition = Networking.LocalPlayer.GetPosition();
+        playerPosition.y = transform.position.y;
     }
 
     private void Update()
@@ -19,13 +20,24 @@ public class FollowPlayerScript : UdonSharpBehaviour
         
        
         playerPosition = Networking.LocalPlayer.GetPosition();
+        playerPosition.y = transform.position.y;
         Vector3 direction = playerPosition - transform.position;
-        direction.Normalize();
-        transform.position += direction * moveSpeed * Time.deltaTime;
         
 
-        
-      
-        
+        float distance = direction.magnitude;
+
+        if (distance > maxDistance)
+        {
+            direction.Normalize();
+            transform.position += direction * moveSpeed * Time.deltaTime;
+
+            // Make the model face the direction it's moving
+            if (direction != Vector3.zero)
+            {
+                transform.LookAt(transform.position + direction);
+            }
+
+        }
+
     }
 }
