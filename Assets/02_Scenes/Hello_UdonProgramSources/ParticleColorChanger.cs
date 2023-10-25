3a9508da991d1dfa0bb2eeb97819909e30d52856
hcbryant9 <bryanthc@vcu.edu>
2023-10-25 19:26:18 -0400

    skybox and particles fade into next color

--- Assets/02_Scenes/Hello_UdonProgramSources/ParticleColorChanger.cs
+++ Assets/02_Scenes/Hello_UdonProgramSources/ParticleColorChanger.cs
@@ -1,30 +1,37 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class ParticleColorChanger : UdonSharpBehaviour
 {
-    //for the particles
+    // For the particles
     public ParticleSystem particleSystem;
     public Gradient[] colorPresets;
+    public float colorChangeDuration = 2.0f; // Adjust the duration as needed
 
-    //for the sky
+    // For the sky
     public Color[] skyColors;
     public Material skyMaterial;
     private int currentColorIndex = 0;
+    private Color currentSkyColor;
+
+    private bool isTransitioning = false;
+    private float transitionStartTime;
+    private int targetColorIndex;
+
     void Start()
     {
         if (particleSystem != null && colorPresets != null)
         {
             ChangeParticleColorPresent(0);
         }
-        if(skyMaterial != null && skyColors != null)
+        if (skyMaterial != null && skyColors != null)
         {
-
+            ChangeSkyColorPresent(0);
         }
     }
+
     public void ChangeParticleColorPresent(int presetIndex)
     {
         if (presetIndex < 0 || presetIndex >= colorPresets.Length)
@@ -32,7 +39,11 @@ public class ParticleColorChanger : UdonSharpBehaviour
             Debug.LogWarning("Invalid preset index");
             return;
         }
+        //stopping,clearing
+        particleSystem.Stop();
+        particleSystem.Clear();
 
+       
         // Access the ColorOverLifetimeModule of the Particle System
         var colorOverLifetime = particleSystem.colorOverLifetime;
 
@@ -41,13 +52,52 @@ public class ParticleColorChanger : UdonSharpBehaviour
 
         //changing the sky
         ChangeSkyColorPresent(presetIndex);
+
+        //okay, we can play again :)
+        particleSystem.Play();
     }
+
     void ChangeSkyColorPresent(int colorIndex)
     {
         if (skyMaterial != null && colorIndex >= 0 && colorIndex < skyColors.Length)
         {
-            skyMaterial.SetColor("_Tint", skyColors[colorIndex]);
-            currentColorIndex = colorIndex;
+            // Store the current sky color and smoothly transition to the new color
+            currentSkyColor = skyMaterial.GetColor("_Tint");
+            targetColorIndex = colorIndex;
+            StartColorTransition(targetColorIndex);
+
+            //stopping particle system and emitting a new color
+            
+            //ChangeParticleColorPresent(colorIndex);
         }
     }
+
+    void Update()
+    {
+        if (isTransitioning)
+        {
+            float t = (Time.time - transitionStartTime) / colorChangeDuration;
+
+            // Lerp between the current and target color
+            Color lerpedColor = Color.Lerp(currentSkyColor, skyColors[targetColorIndex], t);
+
+            // Apply the lerped color to the sky material
+            skyMaterial.SetColor("_Tint", lerpedColor);
+
+            if (t >= 1.0f)
+            {
+                isTransitioning = false;
+            }
+        }
+    }
+
+    void StartColorTransition(int target)
+    {
+        Color targetColor = skyColors[target];
+        currentSkyColor = skyMaterial.GetColor("_Tint");
+        isTransitioning = true;
+        transitionStartTime = Time.time;
+    }
+
+    
 }
--- Assets/02_Scenes/Hello_UdonProgramSources/Trash.cs
+++ Assets/02_Scenes/Hello_UdonProgramSources/Trash.cs
@@ -8,7 +8,7 @@ public class Trash : UdonSharpBehaviour
 {
     public GameManager manager;
     
-    void OnPickUp()
+    void OnPickup()
     {
         //destroy self
         //play sound
